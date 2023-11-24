331bbdf6c5f74321da48e5674e8600b08e24a2a1
Sangng307 <tansang981@gmail.com>
2023-11-24 23:58:48 +0700

    third commit

--- ReactSpring/react-fe-spring/src/page/Register/index.jsx
+++ ReactSpring/react-fe-spring/src/page/Register/index.jsx
@@ -3,38 +3,66 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import { useUser } from "../../component/UserProvider";
 import axios from "axios";
 
-const Login = () => {
+const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const user = useUser();
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageUsername, setErrorMessageUsername] = useState("");
+  const [errorMessagePassword, setErrorMessagePassword] = useState("");
+  const [errorMessageEmail, setErrorMessageEmail] = useState("");
+  function sendRegisterRequest() {
+    // Input validation
+    // if (!username) {
+    //   setErrorMessageUsername("Không được để trống Username.");
+    //   return;
+    // }
+    // if (username.length < 6) {
+    //   setErrorMessageUsername("Username phải trên 6 kí tự.");
+    //   return;
+    // }
+    // if (!password) {
+    //   setErrorMessagePassword("Không được để trống Password.");
+    //   return;
+    // }
+    // if (password.length < 6) {
+    //   setErrorMessagePassword("Password phải trên 6 số.");
+    //   return;
+    // }
 
-  function sendLoginRequest() {
+    // if (!email) {
+    //   setErrorMessageEmail("Không được để trống Email.");
+    //   return;
+    // }
+    // const emailRegex = /^[^\s@]+@gmail\.com$/i;
+    // if (!emailRegex.test(email)) {
+    //   setErrorMessageEmail("Email phải đúng định dạng.");
+    //   return;
+    // }
+
     const reqBody = {
       username: username,
       password: password,
+      email: email,
     };
+
     axios
-      .post("/api/auth/login", reqBody, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
+      .post("/api/auth/register", reqBody)
       .then((response) => {
         if (response.status === 200) {
-          return Promise.all([response.data, response.headers]);
+          setSuccessMessage("User registered successfully!");
         } else {
-          return Promise.reject("Invalid login");
+          setErrorMessage("Trùng Username.");
         }
       })
-      .then(([body, headers]) => {
-        user.setJwt(headers["authorization"]);
-        window.location.href = "/";
-        // console.log(body);
-      })
       .catch((error) => {
-        alert(error.message || "An error occurred during login");
+        console.error("Error registering user: ", error);
+        setErrorMessage("Error registering user. Please try again.");
       });
   }
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
@@ -46,12 +74,15 @@ const Login = () => {
                   Username
                 </label>
                 <input
-                  type="email"
+                  type="text"
                   className="form-control"
                   id="username"
                   value={username}
                   onChange={(event) => setUsername(event.target.value)}
                 />
+                {errorMessageUsername && (
+                  <p className="text-danger">{errorMessageUsername}</p>
+                )}
               </div>
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">
@@ -64,23 +95,48 @@ const Login = () => {
                   value={password}
                   onChange={(event) => setPassword(event.target.value)}
                 />
+                {errorMessagePassword && (
+                  <p className="text-danger">{errorMessagePassword}</p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                {errorMessageEmail && (
+                  <p className="text-danger">{errorMessageEmail}</p>
+                )}
+              </div>
+              <div>
+                {successMessage ? (
+                  <p className="text-success">{successMessage}</p>
+                ) : (
+                  errorMessage && <p className="text-danger">{errorMessage}</p>
+                )}
               </div>
               <div className="d-flex justify-content-end gap-3">
                 <Button
                   id="submit"
                   variant="primary"
-                  onClick={() => sendLoginRequest()}
+                  onClick={() => sendRegisterRequest()}
                 >
-                  Login
+                  Register
                 </Button>
                 <Button
                   id="submit"
                   variant="secondary"
                   onClick={() => {
-                    window.location.href = "/";
+                    window.location.href = "/login";
                   }}
                 >
-                  Back
+                  Login
                 </Button>
               </div>
             </form>
@@ -91,4 +147,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
